2db85e3269355c70febd19c3492aa0a702813328
Your Name <you@example.com>
2026-09-10 13:30:21 +0300

    Ready for vercel deploy

--- api/index.py
+++ api/index.py
@@ -1,1 +1,5 @@
-﻿from backend.main import app
+import sys
+from pathlib import Path
+ROOT_DIR = Path(__file__).resolve().parent.parent
+sys.path.insert(0, str(ROOT_DIR))
+from backend.main import app
